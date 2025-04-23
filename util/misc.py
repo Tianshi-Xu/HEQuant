b01f7788d4a694da034cea9de88408613fb3133d
Tianshi-Xu <994876761@qq.com>
2025-04-23 15:45:44 +0800

    .

--- util/misc.py
+++ util/misc.py
@@ -333,11 +333,11 @@ def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
     return total_norm
 
 
-def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
+def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler,name):
     output_dir = Path(args.output_dir)
     epoch_name = str(epoch)
     if loss_scaler is not None:
-        checkpoint_paths = [output_dir / ("checkpoint-%s.pth" % epoch_name)]
+        checkpoint_paths = [output_dir / ("%s.pth" % name)]
         for checkpoint_path in checkpoint_paths:
             to_save = {
                 "state_dict": model_without_ddp.state_dict(),
@@ -352,7 +352,7 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
         client_state = {"epoch": epoch}
         model.save_checkpoint(
             save_dir=args.output_dir,
-            tag="checkpoint-%s" % epoch_name,
+            tag="%s" % name,
             client_state=client_state,
         )
 

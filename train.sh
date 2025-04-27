9fc29d71a6e36607fa57eb683326b72a6073bffe
Tianshi-Xu <994876761@qq.com>
2025-04-27 18:36:37 +0800

    .

--- train.sh
+++ train.sh
@@ -3,4 +3,6 @@ CUDA_VISIBLE_DEVICES=0 python train_quant.py -c configs/quantized/resnet18/cifar
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nproc_per_node=4 train_quant.py -c configs/quantized/resnet18/imagenet.yml --model ResNet18 /data/dataset/imagenet/
 
+CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --standalone --nproc_per_node=4 train_quant.py -c configs/quantized/resnet18/imagenet.yml --model ResNet18 /data1/share/imagenet
+
 CUDA_VISIBLE_DEVICES=0,1,6,7 python train_imagenet.py -c configs/quantized/resnet18/imagenet.yml --model ResNet18
